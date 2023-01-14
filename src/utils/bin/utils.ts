6c5881e7e4993ba4793b8fb294201daa4662c3cf
FractalLabs0 <122647023+FractalLabs0@users.noreply.github.com>
2023-01-14 16:24:02 -0500

    Update utils.ts

--- src/utils/bin/utils.ts
+++ src/utils/bin/utils.ts
@@ -7,9 +7,6 @@ export const help = async (args: string[]): Promise<string> => {
   return `Available commands:\n${commands}\n\n[tab]\t trigger completion.\n[ctrl+l] clear terminal.\n[ctrl+c] cancel command.`;
 };
 
-export const echo = async (args: string[]): Promise<string> => {
-  return args.join(' ');
-};
 
 export const whoami = async (args: string[]): Promise<string> => {
   return 'adventurer';
@@ -19,28 +16,10 @@ export const date = async (args: string[]): Promise<string> => {
   return new Date().toString();
 };
 
-export const gui = async (args: string[]): Promise<string> => {
-  window.open('https://fractallabs.org', '_self');
-
-  return 'Opening GUI version...';
-};
-
 export const email = async (args: string[]): Promise<string> => {
   window.open('mailto:portal@fractallabs.org');
 
   return 'Opening mailto:portal@fractallabs.org...';
-};
-
-export const vi = async (args: string[]): Promise<string> => {
-  return `why use vi? try 'emacs'.`;
-};
-
-export const vim = async (args: string[]): Promise<string> => {
-  return `why use vim? try 'emacs'.`;
-};
-
-export const emacs = async (args?: string[]): Promise<string> => {
-  return `really? emacs? you should be using 'vim'`;
 };
 
 export const sudo = async (args?: string[]): Promise<string> => {
@@ -49,13 +28,6 @@ export const sudo = async (args?: string[]): Promise<string> => {
   }, 1000);
 
   return `Permission denied: unable to run the command '${args[0]}' as root.`;
-};
-
-
-export const donate = async (args?: string[]): Promise<string> => {
-  window.open(packageJson.funding.url, '_blank');
-
-  return 'Opening donation url...';
 };
 
 export const banner = (args?: string[]): string => {
