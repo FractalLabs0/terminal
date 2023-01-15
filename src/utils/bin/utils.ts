3a5ab20a581354b72ba562a06cc8589bbc8a9cc5
FractalLabs0 <122647023+FractalLabs0@users.noreply.github.com>
2023-01-15 15:01:08 -0500

    Update utils.ts

--- src/utils/bin/utils.ts
+++ src/utils/bin/utils.ts
@@ -1,5 +1,7 @@
 import packageJson from '../../../package.json';
 import * as bin from './index';
+import imageToAdd from "./../assets/images/woofv1.png";
+
 
 export const help = async (args: string[]): Promise<string> => {
   const commands = Object.keys(bin).sort().join(', ');
@@ -7,6 +9,9 @@ export const help = async (args: string[]): Promise<string> => {
   return `Available commands:\n${commands}\n\n[tab]\t trigger completion.\n[ctrl+l] clear terminal.\n[ctrl+c] cancel command.`;
 };
 
+export const sudo = async (args?: string[]): Promise<string> => {
+ return <img src={imageToAdd} alt="Image" />
+};
 
 export const whoami = async (args: string[]): Promise<string> => {
   return 'adventurer';
@@ -22,19 +27,12 @@ export const email = async (args: string[]): Promise<string> => {
   return 'Opening mailto:portal@fractallabs.org...';
 };
 
-export const sudo = async (args?: string[]): Promise<string> => {
-const myImage = new Image(100, 200);
-myImage.src = 'woofv1.png';
-document.body.appendChild(myImage);
-;
 
-  return `Permission denied: unable to run the command '${args[0]}' as root.`;
-};
 
 export const banner = (args?: string[]): string => {
   return `
 
-'<font face="Cascadia">
+'
  ########:'########:::::'###:::::'######::'########::::'###::::'##:::::::
  ##.....:: ##.... ##:::'## ##:::'##... ##:... ##..::::'## ##::: ##:::::::
  ##::::::: ##:::: ##::'##:. ##:: ##:::..::::: ##:::::'##:. ##:: ##:::::::
@@ -43,7 +41,7 @@ export const banner = (args?: string[]): string => {
  ##::::::: ##::. ##:: ##.... ##: ##::: ##:::: ##:::: ##.... ##: ##:::::::
  ##::::::: ##:::. ##: ##:::: ##:. ######::::: ##:::: ##:::: ##: ########:
 ..::::::::..:::::..::..:::::..:::......::::::..:::::..:::::..::........::
-</font>
+
 Type 'help' to see list of available commands.
 
 `;
