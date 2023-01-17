a770a53d6ecd9c124f5e0947011c4a38ec0fd2fc
FractalLabs0 <122647023+FractalLabs0@users.noreply.github.com>
2023-01-17 13:14:16 -0500

    Update social.ts

--- src/utils/bin/social.ts
+++ src/utils/bin/social.ts
@@ -1,6 +1,6 @@
 import config from '../../../config.json';
 
-export const instagram = async (args: string[]): Promise<string> => {
+export const reddit = async (args: string[]): Promise<string> => {
   window.open(`https://www.reddit.com/user/${config.social.reddit}/`);
 
   return 'Opening reddit...';
