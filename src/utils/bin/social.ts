00463916310f8a1dce34f0db7d80649a6b68d818
FractalLabs0 <122647023+FractalLabs0@users.noreply.github.com>
2023-01-17 13:40:41 -0500

    Update social.ts

--- src/utils/bin/social.ts
+++ src/utils/bin/social.ts
@@ -1,13 +1,13 @@
 import config from '../../../config.json';
 
 export const reddit = async (args: string[]): Promise<string> => {
-  window.open(`https://www.reddit.com/user/${config.social.Reddit}/`);
+  window.open(`https://www.reddit.com/user/${config.social.reddit}/`);
 
   return 'Opening reddit...';
 };
 
 export const twitter = async (args: string[]): Promise<string> => {
-  window.open(`https://twitter.com/${config.social.Twitter}/`);
+  window.open(`https://twitter.com/${config.social.twitter}/`);
 
   return 'Opening twitter...';
 };
