c85f85ff4e8220bf0b07402c173b074861bd9d7d
FractalLabs0 <portal@fractallabs.org>
2023-01-17 15:28:09 -0500

    Revert "Update social.ts"
    This reverts commit 00463916310f8a1dce34f0db7d80649a6b68d818.

--- src/utils/bin/social.ts
+++ src/utils/bin/social.ts
@@ -1,13 +1,13 @@
 import config from '../../../config.json';
 
 export const reddit = async (args: string[]): Promise<string> => {
-  window.open(`https://www.reddit.com/user/${config.social.reddit}/`);
+  window.open(`https://www.reddit.com/user/${config.social.Reddit}/`);
 
   return 'Opening reddit...';
 };
 
 export const twitter = async (args: string[]): Promise<string> => {
-  window.open(`https://twitter.com/${config.social.twitter}/`);
+  window.open(`https://twitter.com/${config.social.Twitter}/`);
 
   return 'Opening twitter...';
 };
