367d00537af7b66f7b7b61ddc42a699ba0fe33c8
FractalLabs0 <122647023+FractalLabs0@users.noreply.github.com>
2023-01-17 08:04:15 -0500

    Update utils.ts

--- src/utils/bin/utils.ts
+++ src/utils/bin/utils.ts
@@ -3,7 +3,7 @@ import * as bin from './index';
 import imageToAdd from "./../assets/images/woofv1.png";;
 
 export const help = async (args: string[]): Promise<string> => {
-  const commands = Object.keys(bin).sort().join(', ');
+  const commands = Object.keys(bin).sort().join(',\n ');
 
   return `Available commands:\n${commands}\n\n[tab]\t trigger completion.\n[ctrl+l] clear terminal.\n[ctrl+c] cancel command.`;
 };
