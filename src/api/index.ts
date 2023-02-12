e3776b12eede95e64d6fa620111c2b502eb4aa9c
FractalLabs0 <122647023+FractalLabs0@users.noreply.github.com>
2023-02-12 16:35:05 -0500

    Update index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,2 @@
 import axios from 'axios';
 import config from '../../config.json';
-
-
-//export const getWeather = async (city: string) => {
-  //const { data } = await axios.get(`https://wttr.in/${city}?ATm`);
-
-  //return data;
-//};
-
