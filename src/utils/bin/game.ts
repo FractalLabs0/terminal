a7338d5b85309604edc320a8a2b3e9135d0470d1
FractalLabs0 <portal@fractallabs.org>
2023-02-12 18:26:17 -0500

    Update game.ts

--- src/utils/bin/game.ts
+++ src/utils/bin/game.ts
@@ -1,1 +1,7 @@
+import config from '../../../config.json';
 
+export const instagram = async (args: string[]): Promise<string> => {
+  window.open(`https://www.instagram.com/${config.social.instagram}/`);
+
+  return 'Opening instagram...';
+};
