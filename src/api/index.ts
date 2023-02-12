f08f657007cb76647cf7037d3a7c637ddf994568
FractalLabs0 <portal@fractallabs.org>
2023-02-12 16:39:46 -0500

    Revert "Update index.ts"
    This reverts commit e3776b12eede95e64d6fa620111c2b502eb4aa9c.

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,2 +1,10 @@
 import axios from 'axios';
 import config from '../../config.json';
+
+
+//export const getWeather = async (city: string) => {
+  //const { data } = await axios.get(`https://wttr.in/${city}?ATm`);
+
+  //return data;
+//};
+
