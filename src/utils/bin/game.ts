6c5349354e24f7f6a99e1d166f81ca4e95129836
FractalLabs0 <portal@fractallabs.org>
2023-02-13 11:34:42 -0500

    w

--- src/utils/bin/game.ts
+++
@@ -1,7 +0,0 @@
-import config from '../../../config.json';
-
-export const instagrawem = async (args: string[]): Promise<string> => {
-  window.open(`https://www.instagram.com/${config.social.instagram}/`);
-
-  return 'Opening instagram...';
-};
--- src/utils/bin/index.ts
+++ src/utils/bin/index.ts
@@ -1,3 +1,2 @@
 export * from './social';
 export * from './utils';
-export * from './game';
