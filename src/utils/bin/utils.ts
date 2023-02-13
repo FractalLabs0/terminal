9b6f48c7e86033bcf622c060197b3bbc5eb73c0c
FractalLabs0 <portal@fractallabs.org>
2023-02-13 17:14:31 -0500

    Revert "Update utils.ts"
    This reverts commit 585ef538425a4ca86d400c5252351c8cd51b420c.

--- src/utils/bin/utils.ts
+++ src/utils/bin/utils.ts
@@ -99,26 +99,26 @@ export const go = (direction: string): string => {
 };
 
 export const take = (args: string[]): Promise<string> => {
-  const index = currentLocation.items.indexOf(args[1]);
+  const index = currentLocation.items.indexOf(args[0]);
   if (index === -1) {
     return Promise.resolve("That item isn't here. Current location: " + currentLocation.name);
   }
   takenItems.add(currentLocation.items[index]);
   inventory.push(currentLocation.items[index]);
   currentLocation.items.splice(index, 1);
-  return Promise.resolve(`You took ${args[1]}.`);
+  return Promise.resolve(`You took ${args[0]}.`);
 };
 
-export const drop = (args: string[]): Promise<string> => {
-  const item = args[1];
+
+export const drop = (item: string): string => {
   const index = inventory.indexOf(item);
   if (index === -1) {
-    return Promise.resolve("You don't have that item.");
+    return "You don't have that item.";
   }
   takenItems.delete(item);
   currentLocation.items.push(inventory[index]);
   inventory.splice(index, 1);
-  return Promise.resolve(`You dropped ${item}.`);
+  return `You dropped ${item}.`;
 };
 
 export const adventure = async (args?: string[]): Promise<string> => {
@@ -135,5 +135,7 @@ Start by typing 'adventure go "direction" ' to move to a different location.`;
   switch (args[0]) {
     case "go":
       return go(args[1]);
+    case "drop":
+      return drop(args[1]);
     }
 };
