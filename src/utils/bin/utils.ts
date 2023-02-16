5de9a17c2b7948dc5f059aae4ba64fd567a84802
FractalLabs0 <portal@fractallabs.org>
2023-02-15 19:36:30 -0500

    Update utils.ts

--- src/utils/bin/utils.ts
+++ src/utils/bin/utils.ts
@@ -144,7 +144,7 @@ const displayLocation = (): string => {
   let output = `You are in ${currentLocation.name}. ${currentLocation.description}`;
   const items = currentLocation.items.filter((item) => !takenItems.has(item));
   if (items.length > 0) {
-    output += `\nYou see the following items here: ${items.join(", ")}`;
+    output += `\nYou see the following items here: \x1b[33m${items.join(", ")}\x1b[0m`;
   }
   if (currentLocation.npc) {
     const npc = npcs[currentLocation.npc.toLowerCase()];
