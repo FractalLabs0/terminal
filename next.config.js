47b647f8ec5791c35c9a49e19dd3944a0ed91b6a
FractalLabs0 <portal@fractallabs.org>
2023-03-01 21:06:16 -0500

    w

--- next.config.js
+++ next.config.js
@@ -1,11 +1,18 @@
 
+// next.config.js
 module.exports = {
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.experiments = {
-        topLevelAwait: true,
-      };
-    }
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['next/babel'],
+          plugins: [['@babel/plugin-transform-runtime', { regenerator: true }]],
+        },
+      },
+    });
     return config;
   },
   assetPrefix: '.',
